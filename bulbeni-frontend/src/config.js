398934d96905d4b54ef93b6393fa90fb48b78ad0
ayhan kemal <ayhankemalbaykal@yahoo.com>
2024-12-13 02:19:16 +0300

    BULON-7 Configuration with .env files

--- bulbeni-frontend/src/config.js
+++ bulbeni-frontend/src/config.js
@@ -1,8 +1,5 @@
-// src/config.js
 const config = {
-  API_URL: 'http://localhost:5000'
-  
-  //API_URL: 'http://18.194.177.88:5000'
+  API_URL: process.env.REACT_APP_BULBENI_API_BACKEND_URL || 'http://localhost:5000'
 };
 
-export default config; 
+export default config;
